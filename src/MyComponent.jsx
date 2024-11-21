e2d71f24ad320e931e79bf0c5babf2ef21bf58a6
bry <bryanong.23@ichat.sp.edu.sg>
2024-11-21 12:55:59 +0800

    commit

---
+++ src/MyComponent.jsx
@@ -0,0 +1,8 @@
+// src/MyComponent.jsx
+import React from 'react';
+
+const MyComponent = () => {
+  return <h1>Hello from MyComponent!</h1>;
+};
+
+export default MyComponent;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
+// src/index.js
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import MyComponent from './MyComponent'; // Import your JSX file
 
-const myFirstElement = <h1>Hello React!</h1>
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(myFirstElement);
+ReactDOM.render(
+  <MyComponent />,
+  document.getElementById('root')
+);
