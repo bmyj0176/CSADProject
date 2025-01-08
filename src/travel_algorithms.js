2223e6bbc47227dfc674cb60d800caa0da97f0de
caleb1607 <awsomeong@gmail.com>
2025-01-09 02:39:23 +0800

    dabinky code update, 2am coding not it

--- src/travel_algorithms.js
+++ src/travel_algorithms.js
@@ -33,22 +33,35 @@ async function test() {
 console.clear();
 let train_transfer = await get_json('./datasets/platform.json');
 let train_paths = await get_json('./datasets/station.json');
-let trains2 = new Object();
-let trains = train_paths[0]["stations"];
-let map = buildAdjacencyList(trains);
+let map = buildAdjacencyList(train_paths, train_transfer);
+console.log(map);
 
 
-function buildAdjacencyList(connections) {
+function buildAdjacencyList(time_between_stations, connections) {
+
     const adjMap = {};
 
-    for (const { s1, s2, time } of connections) {
+    for (let i in time_between_stations){ //adding time between stations
+        let x = time_between_stations[i]["stations"]
+        for (const { s1, s2, time } of x) {
+            // Add connection from s1 to s2
+            if (!adjMap[s1]) adjMap[s1] = {};
+            adjMap[s1][s2] = time;
+    
+            // Add connection from s2 to s1 (assuming undirected graph)
+            if (!adjMap[s2]) adjMap[s2] = {};
+            adjMap[s2][s1] = time;
+        }
+    }
+    
+    for (const { p1, p2, time } of connections) { //adding cross platform transfers
         // Add connection from s1 to s2
-        if (!adjMap[s1]) adjMap[s1] = {};
-        adjMap[s1][s2] = time;
+        if (!adjMap[p1]) adjMap[p1] = {};
+        adjMap[p1][p2] = time;
 
         // Add connection from s2 to s1 (assuming undirected graph)
-        if (!adjMap[s2]) adjMap[s2] = {};
-        adjMap[s2][s1] = time;
+        if (!adjMap[p2]) adjMap[p2] = {};
+        adjMap[p2][p1] = time;
     }
 
     return adjMap;
@@ -57,8 +70,7 @@ function buildAdjacencyList(connections) {
 export function dijkstra(graph, start, end) {
     // Create an object to store the shortest distance from the start node to every other node
     let distances = {};
-
-    // A set to keep track of all visited nodes
+    let predecessors = {}; // Map to store the predecessor of each node for route reconstruction
     let visited = new Set();
 
     // Get all the nodes of the graph
@@ -67,6 +79,7 @@ export function dijkstra(graph, start, end) {
     // Initially, set the shortest distance to every node as Infinity except starting node
     for (let node of nodes) {
         distances[node] = Infinity;
+        predecessors[node] = null; // No predecessor initially
     }
     distances[start] = 0;
 
@@ -80,31 +93,68 @@ export function dijkstra(graph, start, end) {
         if (distances[currentNode] === Infinity) break;
 
         // Mark the chosen node as visited
-        visited.add(currentNode); 
+        visited.add(currentNode);
 
-        // For each neighbouring node of the current node
+        // For each neighboring node of the current node
         for (let neighbour in graph[currentNode]) {
-            // If the neighbour hasn't been visited yet
             if (!visited.has(neighbour)) {
                 // Calculate tentative distance to the neighbouring node
                 let newDistance = distances[currentNode] + graph[currentNode][neighbour];
 
                 // If the newly calculated distance is shorter than the previously known distance to this neighbour
                 if (newDistance < distances[neighbour]) {
-                    // Update the shortest distance to this neighbour
                     distances[neighbour] = newDistance;
+                    predecessors[neighbour] = currentNode; // Update predecessor
                 }
             }
         }
     }
 
-    // Return the shortest distance from the start node to all nodes
-    let output = "Time from " + start + " to " + end + " is " + distances[end] + " min.";
-    return output;
+    // If the end node is unreachable
+    if (distances[end] === Infinity) {
+        return `No route from ${start} to ${end}.`;
+    }
+
+    // Reconstruct the shortest route from start to end using the predecessors map
+    let route = [];
+    let current = end;
+    while (current) {
+        route.unshift(current);
+        current = predecessors[current];
+    }
+
+    //showing the route as only transfers
+    let simple_route = [route[0]];
+    let past_station = "";
+    let past_interchange = "";
+
+    for (let i in route) {
+        let current_station = route[i];
+        let thirdLastChar = current_station.charAt(current_station.length - 3);
+
+        if (thirdLastChar == "_") { //if third last char is a "_", its an interchange
+            let current_interchange = current_station.length > 3 ? current_station.slice(0, -3) : "";
+            if (current_interchange == past_interchange) { //if 2 interchanges are the same, its a transfer
+                simple_route.push(past_station, current_station); 
+            } else {
+                past_interchange = current_interchange;
+            }
+        }
+        past_station = current_station;
+    }
+
+    simple_route.push(route[route.length - 1]) //add destination to simple route
+
+    // Return both the shortest distance and the route
+    return {
+        "time taken": distances[end] + " minutes",
+        route: route,
+        simple_route: simple_route
+    };
 }
 
 // Example: Find shortest distances from node A to all other nodes in the graph
-console.log(dijkstra(map, "Woodlands_NS", "Choa Chu Kang_NS")); // Outputs: {8}
+console.log(dijkstra(map, "Marina South Pier", "Phoenix")); // Outputs: {8}
 
 export async function shortest_path (station1, station2) {
     let path = []
