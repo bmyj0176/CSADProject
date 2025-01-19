7a6ca8de56f9569aff0f9f8cf7658e001d768bd1
bry <bryanong.23@ichat.sp.edu.sg>
2025-01-19 22:25:15 +0800

    did i do anythhhing

--- src/pages/ArrivalTimes/ATSearchResult.js
+++ src/pages/ArrivalTimes/ATSearchResult.js
@@ -78,23 +78,20 @@ const ATSearchResult = (props) => {
 const StarButton = (props) => {
   return (
     <button // STAR BUTTONNNNNNNNN
-        id={props.subheader1 ? 'buttonchange' : 'buttonchange2'}
-        className={props.favItem === 'selected' ? "btnfaved" : "btnunfaved"}
-        onClick={props.handleFav}
-        type="button"
-      >   
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 24 24"
-          width="24"
-          height="24"
-        >
-          <path
-            d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
-            fill="currentColor"
-          />
-        </svg>
-      </button>
+      id={props.subheader1 ? 'buttonchange' : 'buttonchange2'}
+      className={props.favItem === 'selected' ? "btnfaved" : "btnunfaved"}
+      onClick={props.handleFav}
+      type="button">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24">
+        <path
+          d="M12 17.27L18.18 21l-1.64-7.03L22 9.24l-7.19-.61L12 2 9.19 8.63 2 9.24l5.46 4.73L5.82 21z"
+          fill="currentColor"/>
+      </svg>
+    </button>
   )
 }
 
