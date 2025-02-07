57c42293e55b925b0b3ca545fa680877dfd042d7
bry <bryanong.23@ichat.sp.edu.sg>
2025-02-07 09:36:47 +0800

    bombed an mrt station for announcement page to work (it did for like 5min)

--- src/pages/Announcements.js
+++ src/pages/Announcements.js
@@ -14,9 +14,15 @@ const Announcements  = ({ isAdmin }) => {
   useEffect(() => {
     const fetchTrainAlerts = async () => {
       try {
-        const response = await TrainAlertsService();
-        console.log(response);
-        setAlerts(response.data);
+        const data = await TrainAlertsService();
+        const messages = data.value.Message
+        let message = "";
+        for (const announcement of messages)
+        {
+          message += announcement.Content;
+        }
+        console.log(message);
+        setAlerts(data.value);
       } catch (error) {
         setError("Failed to fetch train alerts. Please try again later.");
         console.error("API error:", error);
@@ -100,18 +106,18 @@ const Announcements  = ({ isAdmin }) => {
         </div>
       )}
       <h1>Train Service Alerts</h1>
-      {alerts?.Status === 1 ? (
+      {console.log(alerts?.Message.length)}
+      {alerts?.Message.length === 0 ? (
         <p style={{ color: "green" }}>All train services are running smoothly.</p>
       ) : (
         <div>
           <h2>Alerts:</h2>
-          {alerts?.AffectedSegments.length > 0 ? (
+          {alerts?.Message.length > 0 ? (
             <ul>
-              {alerts.AffectedSegments.map((segment, index) => (
+              {alerts.Message.map((msg, index) => (
                 <li key={index}>
-                  <strong>Line:</strong> {segment.Line} <br />
-                  <strong>Direction:</strong> {segment.Direction} <br />
-                  <strong>Status:</strong> {segment.Status}
+                  <strong>Message:</strong> {msg.Content} <br />
+                  <strong>Date:</strong> {msg.CreatedDate}
                 </li>
               ))}
             </ul>
